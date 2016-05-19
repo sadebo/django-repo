645f1041efaf28ef3e3d4b5d5377d7ca55e8fa32
root <root@localhost.localdomain>
2016-05-19 13:04:29 -0400

    Django ready

---
+++ src/todo/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from todo import views
+from rest_framework.routers import DefaultRouter
+
+# create a router and register our viewsets with it
+router = DefaultRouter(trailing_slash=False)
+router.register(r'todos', views.TodoItemViewSet)
+
+# The API  URLs  are now determined 
+
+urlpatterns = [
+	url(r'^', include(router.urls)),
+]
